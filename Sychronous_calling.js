ba649a6b7ff26bbb29bfe21f9cb8569185bcf43e
DYNAMIC_DISHANK <138884609+DISHANK-PATEL@users.noreply.github.com>
2024-07-10 02:09:43 +0530

    Create Sychronous_calling.js

---
+++ Sychronous_calling.js
@@ -0,0 +1,21 @@
+function task1()
+{
+    console.log("TASK1 IS COMPLETED");
+}
+function task2()
+{
+    console.log("TASK2 IS COMPLETED");
+}
+function task3()
+{
+    console.log("TASK3 IS COMPLETED");
+}
+function task4()
+{
+    console.log("TASK4 IS COMPLETED");
+}
+task1();    // Currently all the functions are Sychronous hence are executed without much problem
+task2();
+task3();
+task4();
+console.log("All tasks are completed");
